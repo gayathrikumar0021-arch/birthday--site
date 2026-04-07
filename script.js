54a10f962a401d1d52477a976d84c202d706e265
gayathrikumar0021-arch <gayathrikumar0021@gmail.com>
2026-04-07 16:46:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showSurprise() {
+    document.getElementById("surprise").style.display = "block";
+
+    let music = document.getElementById("music");
+    music.play();
+
+    alert("🎉 Happy Birthday! 🎂");
+}
